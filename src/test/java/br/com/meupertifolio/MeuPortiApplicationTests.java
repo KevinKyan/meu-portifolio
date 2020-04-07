5d5299cb82d5446228a015cb628eaa64561edcba
Kevin <kevinkyan1004@gmail.com>
2020-04-07 10:01:57 -0300

    Primeiro commit - meu Portifolio

---
+++ src/test/java/br/com/meupertifolio/MeuPortiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meupertifolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPortiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
